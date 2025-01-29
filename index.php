311853d0a5be5519366a4467a025d5c8eb1d4d44
parthiban-alt <ps4972083@gmail.com>
2025-01-29 12:32:57 +0530

    upload

--- index.php
+++ index.php
@@ -1,3 +1,59 @@
+<?php
+if(isset($_REQUEST["uploadResumeBtn"]))
+{
+    $target_dir = "upload/";
+    //$target_file = $target_dir . basename($_FILES["resume"]["name"]);
+    $filename = $_FILES["resume"]["name"];
+    $extension = explode(".", $filename);
+    echo $target_file = $target_dir . rand().".". $extension[1];
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["uploadResumeBtn"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+    
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["resume"]["size"] > 50000000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" && $imageFileType != "pdf") {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["resume"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["resume"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,10 +63,10 @@
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <form action=" " method="post" enctype="multipart/form-data">
          Select image to upload:
-         <input type="file" name="fileToUpload" id="fileToUpload">
-         <input type="submit" value="Upload Image" name="submit">
+         <input type="file" name="resume" id="resume">
+         <input type="submit" value="Upload Now" name="uploadResumeBtn">
     </form>
 </body>
 </html>
